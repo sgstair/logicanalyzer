16c80083375d3bca8c31759d1bcbce50b6fd5469
Stephen Stair <sgstair@akkit.org>
2014-06-01 15:50:06 -0700

    Starting FPGA project

---
+++ fpga/toplevel.vhd
@@ -0,0 +1,44 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    15:44:56 06/01/2014 
+-- Design Name: 
+-- Module Name:    toplevel - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity toplevel is
+    Port ( clk : in  STD_LOGIC;
+           ledred : out  STD_LOGIC;
+           ledgreen : out  STD_LOGIC);
+end toplevel;
+
+architecture Behavioral of toplevel is
+
+begin
+
+
+end Behavioral;
+
